dd6bafa084436507cd83aac30b88fd187fa17f77
Paul Tavares <paul.tavares@elastic.co>
2023-03-23 15:51:35 -0400

    style cards on main page

--- public/pages/home/views/home_page.tsx
+++ public/pages/home/views/home_page.tsx
@@ -1,7 +1,13 @@
 import React, { memo } from 'react';
-import { EuiFlexItem, EuiFlexGroup } from '@elastic/eui';
+import { EuiFlexItem, EuiFlexGroup, EuiSpacer } from '@elastic/eui';
+import styled from 'styled-components';
 import { AppPage } from '../../../components/app_page';
 import { ApiMateCard } from '../../api_mate';
+
+const CardContainer = styled.div`
+  width: 25%;
+  min-width: 350px;
+`;
 
 export interface HomePageProps {
   // TODO: define props
@@ -13,9 +19,12 @@ export const HomePage = memo<HomePageProps>((props) => {
       pageTitle="Endpoint Developer Tools"
       description="Various web based tools to help in development"
     >
+      <EuiSpacer size="xl" />
       <EuiFlexGroup wrap>
         <EuiFlexItem>
-          <ApiMateCard />
+          <CardContainer>
+            <ApiMateCard />
+          </CardContainer>
         </EuiFlexItem>
       </EuiFlexGroup>
     </AppPage>
