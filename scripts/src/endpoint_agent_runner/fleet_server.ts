1c1133622a4e156fc6cd2d1858e1bf4b52a048ba
Paul Tavares <paul.tavares@elastic.co>
2023-08-14 09:49:09 -0400

    increase time to wait for fleet server to show up

--- scripts/src/endpoint_agent_runner/fleet_server.ts
+++ scripts/src/endpoint_agent_runner/fleet_server.ts
@@ -250,7 +250,7 @@ export const startFleetServerWithDocker = async ({
 
     containerId = (await execa('docker', dockerArgs)).stdout;
 
-    const fleetServerAgent = await waitForHostToEnroll(kbnClient, containerName);
+    const fleetServerAgent = await waitForHostToEnroll(kbnClient, containerName, 60000);
 
     log.verbose(`Fleet server enrolled agent:\n${JSON.stringify(fleetServerAgent, null, 2)}`);
 
