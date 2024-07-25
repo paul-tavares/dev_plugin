21c3c06384d28309903b2176210b4800dde8d1f7
Paul Tavares <paul.tavares@elastic.co>
2024-07-25 08:59:07 -0400

    Fix EuiProvider warning

--- public/components/app.tsx
+++ public/components/app.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { I18nProvider } from '@kbn/i18n-react';
 import { Router } from '@kbn/shared-ux-router';
 import { KibanaContextProvider } from '@kbn/kibana-react-plugin/public';
-
+import { EuiThemeProvider } from '@kbn/kibana-react-plugin/common';
 import { CoreStart, ScopedHistory } from '@kbn/core/public';
-
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { KibanaRenderContextProvider } from '@kbn/react-kibana-context-render';
+import useObservable from 'react-use/lib/useObservable';
 import { AppRoutes } from './app_routes';
 import { PLUGIN_NAME } from '../../common';
 import { AppPluginStartDependencies } from '../types';
@@ -34,22 +35,32 @@ export const DevPluginApp = ({
   coreStart,
   history,
 }: DevPluginAppDeps) => {
+  const { theme: themeStart } = coreStart;
+  const theme = useObservable(themeStart.theme$, themeStart.getTheme());
+  const isDarkMode = theme.darkMode;
+
+  const services = useMemo(() => {
+    return {
+      appName: PLUGIN_NAME,
+      startDependencies,
+      appBasePath,
+      ...coreStart,
+    };
+  }, [appBasePath, coreStart, startDependencies]);
+
   return (
-    <KibanaContextProvider
-      services={{
-        appName: PLUGIN_NAME,
-        startDependencies,
-        appBasePath,
-        ...coreStart,
-      }}
-    >
-      <QueryClientProvider client={queryClient}>
-        <I18nProvider>
-          <Router history={history}>
-            <AppRoutes />
-          </Router>
-        </I18nProvider>
-      </QueryClientProvider>
-    </KibanaContextProvider>
+    <KibanaRenderContextProvider {...coreStart}>
+      <KibanaContextProvider services={services}>
+        <EuiThemeProvider darkMode={isDarkMode}>
+          <QueryClientProvider client={queryClient}>
+            <I18nProvider>
+              <Router history={history}>
+                <AppRoutes />
+              </Router>
+            </I18nProvider>
+          </QueryClientProvider>
+        </EuiThemeProvider>
+      </KibanaContextProvider>
+    </KibanaRenderContextProvider>
   );
 };
